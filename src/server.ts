6af4559adf75055453bdea2a46efb3599ff6a31a
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-23 02:21:36 -0300

    Aula 1 �

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express ();
+
+
+app.get("/users", (request, response) => {
+    
+    return response.json({message: "Hello World - NLW04"});
+})
+
+app.post ("/", (request, response) => {
+    // Recebeu os dados para salvar
+    return response.json({message: "Os dados foram salvos com sucesso"});
+});
+
+app.listen(3333, () => console.log("Server is running!"));
+
