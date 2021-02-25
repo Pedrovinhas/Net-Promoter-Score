246ee7029b59279d64a987b57c2a9bbd574c90ff
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-25 12:44:11 -0300

    Aula 03 - Testando nossa aplicação ⛏

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from "../models/User";
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UserRepository);
 
         // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
         const userAlreadyExists = await usersRepository.findOne({ 
@@ -26,8 +26,8 @@ class UserController {
 
         await usersRepository.save (user);
 
-        return response.json()
+        return response.status(201).json(user);
     }
 }
 
-export { UserController }
+export { UserController };
